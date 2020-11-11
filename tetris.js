ab357b2a7692bfdc3b0f571b4785da3ebdd90b01
AndyANZHENG <zheng.a@yahoo.com>
2020-11-11 09:55:07 +0800

    Create drawMatrix function

--- tetris.js
+++ tetris.js
@@ -4,5 +4,26 @@ const canvas = document.getElementById("tetris");
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext("2d");
 
+ctx.scale(20, 20);
+
 ctx.fillStyle = "#000";
-ctx.fillRect(50, 50, canvas.width, canvas.height);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const matrix = [
+  [0, 0, 0],
+  [1, 1, 1],
+  [0, 1, 0],
+];
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, rowIdx) => {
+    row.forEach((col, colIdx) => {
+      if (col !== 0) {
+        ctx.fillStyle = "green";
+        ctx.fillRect(colIdx + offset.x, rowIdx + offset.y, 1, 1);
+      }
+    });
+  });
+}
+
+drawMatrix(matrix, { x: 1, y: 3 });
