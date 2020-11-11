849a9f74938657d4cd3bfc5eed0fdbbdc6b5f6cf
AndyANZHENG <zheng.a@yahoo.com>
2020-11-11 23:10:10 +0800

    make it drop every second

--- tetris.js
+++ tetris.js
@@ -3,16 +3,24 @@ const matrix = [
   [1, 1, 1],
   [0, 1, 0],
 ];
+const sideLength = 20;
+// one second
+const dropInterval = 1000;
 
+let dropTimer = 0;
 let player = { pos: { x: 5, y: 5 }, matrix };
-const width = 20;
 
 function drawMatrix(matrix, offset) {
   matrix.forEach((row, rowIdx) => {
     row.forEach((col, colIdx) => {
       if (col !== 0) {
         fill("green");
-        rect(colIdx * width + offset.x, rowIdx * width + offset.y, 20, 20);
+        rect(
+          colIdx * sideLength + offset.x,
+          rowIdx * sideLength + offset.y,
+          20,
+          20
+        );
       }
     });
   });
@@ -24,5 +32,10 @@ function setup() {
 
 function draw() {
   background(0);
+  dropTimer += deltaTime;
+  if (dropTimer > dropInterval) {
+    player.pos.y += sideLength;
+    dropTimer = 0;
+  }
   drawMatrix(matrix, player.pos);
 }
