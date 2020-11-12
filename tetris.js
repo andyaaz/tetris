7fbcbca2ff587f34f2a9b2ec1361ec4b9222c977
AndyANZHENG <zheng.a@yahoo.com>
2020-11-13 00:16:05 +0800

    add createPiece function

--- tetris.js
+++ tetris.js
@@ -11,6 +11,52 @@ let dropTimer = 0;
 let player = { pos: { x: 0, y: 0 }, matrix };
 const arena = createMatrix(20, 20);
 
+function createPiece(type) {
+  if (type === "I") {
+    return [
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+    ];
+  } else if (type === "L") {
+    return [
+      [0, 2, 0],
+      [0, 2, 0],
+      [0, 2, 2],
+    ];
+  } else if (type === "J") {
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [3, 3, 0],
+    ];
+  } else if (type === "O") {
+    return [
+      [4, 4],
+      [4, 4],
+    ];
+  } else if (type === "Z") {
+    return [
+      [5, 5, 0],
+      [0, 5, 5],
+      [0, 0, 0],
+    ];
+  } else if (type === "S") {
+    return [
+      [0, 6, 6],
+      [6, 6, 0],
+      [0, 0, 0],
+    ];
+  } else if (type === "T") {
+    return [
+      [0, 7, 0],
+      [7, 7, 7],
+      [0, 0, 0],
+    ];
+  }
+}
+
 function createMatrix(w, h) {
   return [...Array(w).keys()].map(() => new Array(h).fill(0));
 }
